93746e0ad84f117e47d7c40f5c49b76099f4c660
John Rizzo <johnrizzo1@gmail.com>
2024-09-17 09:37:03 -0400

    initial setup

---
+++ devenv.nix
@@ -0,0 +1,23 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [ pkgs.git pkgs.hugo ];
+  enterShell = ''
+    git --version
+  '';
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+
+  languages.nix.enable = true;
+  languages.go.enable = true;
+  languages.python.enable = true;
+  languages.python.venv.enable = true;
+  languages.python.venv.requirements = ./requirements.txt;
+  languages.javascript.enable = true;
+  languages.javascript.npm.enable = true;
+  languages.typescript.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
